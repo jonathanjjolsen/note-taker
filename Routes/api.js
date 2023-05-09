f0a2636bbdff62b174b635ff13f2cb750e127b46
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-05-08 18:19:09 -0700

    Fixed bug

--- Routes/api.js
+++ Routes/api.js
@@ -1,8 +1,8 @@
 const routes = require('express').Router();
 const fs = require('fs');
 
-routes.get('/api/notes', (req, res) => {
-const notesdb = JSON.parse(fs.readFile('db/db.json', 'utf8'));
+routes.get('/api/notes', async (req, res) => {
+const notesdb = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 res.json(notesdb);
 })
 
