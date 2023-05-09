2d518b5fb8369a847773d14486d4d6140d0103d5
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-05-08 17:33:22 -0700

    Added api routes

--- Routes/api.js
+++ Routes/api.js
@@ -1,1 +1,9 @@
 const routes = require('express').Router();
+const fs = require('fs');
+
+routes.get('/api/notes', (req, res) => {
+const notesdb = JSON.parse(fs.readFile('db/db.json', 'utf8'));
+res.json(notesdb);
+})
+
+module.exports = routes;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const htmlRoutes = require('./Routes/html.js')
+const apiRoutes = require('./Routes/api.js')
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,5 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 app.use(htmlRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
