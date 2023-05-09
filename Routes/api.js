4c1c059ea0acf8f3a58882caf2a50fc31844f280
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-05-08 21:56:58 -0700

    Added post route, and uuid.js

--- Routes/api.js
+++ Routes/api.js
@@ -6,4 +6,16 @@ const notesdb = await JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
 res.json(notesdb);
 })
 
+routes.post('/api/notes', (req, res) => {
+    const notesdb = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuid(),
+    };
+    notesdb.push(newNote);
+    fs.writeFileSync('db/db.json', JSON.stringify(notesdb));
+    res.json(notesdb);
+})
+
 module.exports = routes;
